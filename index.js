9d6dbd5a62fff5e019b8c1a5034667ead0af3baf
benhayehudi <bengreenberg@gmail.com>
2020-03-08 00:00:26 +0200

    google maps

--- index.js
+++ index.js
@@ -84,10 +84,10 @@ Toolkit.run(async tools => {
       }).then(d => { console.log(`data: ${JSON.stringify(d)}`) })
 
       // Assign the date and time in the user's location to the date_time variable
-      date_time = new Date(tz.local_timestamp * 1000);
-      date_string = date_time.toDateString() + ' - ' + date_time.getHours() + ':' + date_time.getMinutes();
-      console.log(tz.local_timestamp)
-      console.log(date_string)
+      // date_time = new Date(tz.local_timestamp * 1000);
+      // date_string = date_time.toDateString() + ' - ' + date_time.getHours() + ':' + date_time.getMinutes();
+      // console.log(tz.local_timestamp)
+      // console.log(date_string)
 
       const responseMsg = `
         Hi there, ${actor}! 👋
